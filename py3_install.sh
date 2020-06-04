323dddcee5e44901794a9d3f805c921f3e32dfdd
Vincent <985126792@qq.com>
2020-06-04 19:05:40 +0800

    Update py3_install.sh

--- py3_install.sh
+++ py3_install.sh
@@ -7,8 +7,8 @@ cd Python-3.7.2 &&
 #第一个指定安装的路径,不指定的话,安装过程中可能软件所需要的文件复制到其他不同目录,删除软件很不方便,复制软件也不方便.
 #第二个可以提高python10%-20%代码运行速度.
 #第三个是为了安装pip需要用到ssl,后面报错会有提到.
-make && make install &&
 yum install libffi-devel &&
+make && make install &&
 ln -s /usr/local/python3/bin/python3 /usr/local/bin/python3 &&
 ln -s /usr/local/python3/bin/pip3 /usr/bin/pip3 &&
 python3 -V &&
